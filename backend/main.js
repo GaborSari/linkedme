03b1900b6abc59375005b273dff9f7fb0093541a
gara22 <gomold22@gmail.com>
2019-04-23 17:31:40 +0200

    login elvileg működik

--- backend/main.js
+++ backend/main.js
@@ -3,27 +3,26 @@ var cors = require('cors');
 const app = express();
 const port = 3000;
 
+let user = {
+	uname: '',
+	pw: '',
+	isCompany: false
+};
+let response = {
+	success: false,
+	username: '',
+	isCompany: false
+};
+
 var oracledb = require('oracledb');
-oracledb.getConnection(
-	{
-		user: 'h652913',
-		password: 'h652913',
-		connectString: 'hostname:orania.inf.u-szeged.hu' // ez nemtom jó-e
-	},
-	function(err, connection) {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		connection.execute('SELECT *' + 'FROM jobs ', function(err, result) {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			console.log(result.rows);
-		});
-	}
-);
+
+function checkCompany(p) {
+	return p || p == undefined;
+}
+
+function checkUser(name1, name2, pw1, pw2) {
+	return name1 == name2 && pw1 == pw2;
+}
 
 app.use(
 	cors({
@@ -41,7 +40,86 @@ app.get('/', function(req, res) {
 
 // POST method route
 app.post('/login', function(req, res) {
+	user.uname = req.body.username;
+	user.pw = req.body.password;
+	if (req.body.company == undefined || req.body.company == false) {
+		user.isCompany = false;
+	} else user.isCompany = true;
+
+	// user.isCompany = res.body.company;
 	console.log(req.body);
+
+	// console.log(user);
+
+	oracledb.getConnection(
+		{
+			user: 'h776447',
+			password: 'h776447',
+			connectString:
+				'(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 4000))(CONNECT_DATA =(SID= kabinet)))' // ez nemtom jó-e
+		},
+		function(err, connection) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+
+			if (!user.isCompany) {
+				connection.execute(`SELECT username, password FROM seekers where USERNAME = '${user.uname}'`, function(
+					err,
+					result
+				) {
+					if (err) {
+						console.error(err);
+						return;
+					}
+					res = result.rows;
+					console.log(user);
+
+					if (res.length > 0 && checkUser(user.uname, res[0][0], user.pw, res[0][1])) {
+						response.success = true;
+						response.username = user.username;
+						response.isCompany = false;
+						console.log('sikeres bejelentkezés');
+					} else {
+						response.success = false;
+						response.username = '';
+						response.isCompany = false;
+						console.log('sikertelen bejelentkezés');
+					}
+
+					console.log(res[0]);
+				});
+			} else {
+				connection.execute(
+					`SELECT username, password FROM companies where USERNAME = '${user.uname}'`,
+					function(err, result) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						res = result.rows;
+						console.log(user);
+
+						if (res.length > 0 && checkUser(user.uname, res[0][0], user.pw, res[0][1])) {
+							response.success = true;
+							response.username = user.username;
+							response.isCompany = false;
+							console.log('sikeres bejelentkezés');
+						} else {
+							response.success = false;
+							response.username = '';
+							response.isCompany = false;
+							console.log('sikertelen bejelentkezés');
+						}
+
+						console.log(res[0]);
+					}
+				);
+			}
+		}
+	);
+	res.json(response);
 });
 
 app.post('/registration', function(req, res) {
