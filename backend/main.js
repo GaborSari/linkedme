f92d29c0182a1a67ce42666fcff00c08e0fb39d5
gara22 <gomold22@gmail.com>
2019-04-23 18:29:41 +0200

    reg félig szar

--- backend/main.js
+++ backend/main.js
@@ -8,6 +8,21 @@ let user = {
 	pw: '',
 	isCompany: false
 };
+let newUser = {
+	uname: '',
+	pw: '',
+	birth: '',
+	cv: ''
+};
+
+let newCompany = {
+	username: '',
+	isCompany: true,
+	password: '',
+	address: '',
+	webpage: '',
+	details: ''
+};
 let response = {
 	success: false,
 	username: '',
@@ -15,10 +30,6 @@ let response = {
 };
 
 var oracledb = require('oracledb');
-
-function checkCompany(p) {
-	return p || p == undefined;
-}
 
 function checkUser(name1, name2, pw1, pw2) {
 	return name1 == name2 && pw1 == pw2;
@@ -123,7 +134,115 @@ app.post('/login', function(req, res) {
 });
 
 app.post('/registration', function(req, res) {
-	console.log(req.body);
+	if (req.body.isCompany == undefined || req.body.isCompany == false) {
+		newUser.uname = req.body.username;
+		newUser.pw = req.body.password;
+		newUser.cv = req.body.cv;
+		newUser.birth = req.body.birth;
+	} else {
+		newCompany.username = req.body.username;
+		newCompany.isCompany = req.body.isCompany;
+		newCompany.password = req.body.password;
+		newCompany.address = req.body.address;
+		newCompany.webpage = req.body.webpage;
+		newCompany.details = req.body.details;
+	}
+
+	console.log(newUser);
+	console.log(newCompany);
+
+	oracledb.getConnection(
+		{
+			user: 'h776447',
+			password: 'h776447',
+			connectString:
+				'(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 4000))(CONNECT_DATA =(SID= kabinet)))' // ez nemtom jó-e
+		},
+		function(err, connection) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+
+			if (
+				newUser.uname.length > 0 &&
+				newUser.pw.length > 0 &&
+				newUser.cv.length > 0 &&
+				newUser.birth.length > 0
+			) {
+				connection.execute(
+					`SELECT username, password FROM seekers where USERNAME = '${newUser.uname}'`,
+					function(err, result) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						res = result.rows;
+
+						if (res.length > 0) {
+							response.success = false;
+
+							console.log('sikertelen regisztráció');
+						} else {
+							connection.execute(
+								`INSERT INTO   seekers (username, password, birth, cv)  VALUES('${newUser.uname}', '${newUser.pw}', '${newUser.birth}', '${newUser.cv}')`,
+								{ autoCommit: true },
+								function(err, result) {
+									if (err) {
+										console.error(err);
+										return;
+									}
+									// res = result.rows;
+								}
+							);
+							response.success = true;
+							console.log('sikeres regisztráció');
+						}
+					}
+				);
+			} else if (
+				newCompany.username.length > 0 &&
+				newCompany.password.length > 0 &&
+				newCompany.webpage.length > 0 &&
+				newCompany.address.length > 0 &&
+				newCompany.details.length > 0
+			) {
+				connection.execute(
+					`SELECT username, password FROM companies where USERNAME = '${newUser.uname}'`,
+					function(err, result) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						res = result.rows;
+
+						if (res.length > 0) {
+							response.success = false;
+
+							console.log('sikertelen regisztráció');
+							return;
+						} else {
+							connection.execute(
+								`INSERT INTO   companies (username, password, address, webpage, details)  VALUES('${newCompany.username}', '${newCompany.password}', '${newCompany.address}', '${newCompany.webpage}', '${newCompany.details}')`,
+								{ autoCommit: true },
+								function(err, result) {
+									if (err) {
+										console.error(err);
+										return;
+									}
+									// res = result.rows;
+								}
+							);
+							response.success = true;
+							console.log('sikeres regisztráció');
+						}
+					}
+				);
+			}
+		}
+	);
+	res.json(response);
+	// console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
